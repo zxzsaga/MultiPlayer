a25d8282875cd59937caf3faa9894d1e67be7cf1
Xuanzhi Zhang <xuanzhi.zhang@dena.com>
2016-05-26 18:21:11 +0800

    添加 Move 和 Follow 两个脚本

---
+++ Assets/Driver.cs
@@ -0,0 +1,21 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Driver : MonoBehaviour {
+
+	public float velocity = 0.02f;
+	private Vector3 direction = Vector3.forward;
+
+	static Quaternion rotationLeft  = Quaternion.Euler(0, -5, 0);
+	static Quaternion rotationRight = Quaternion.Euler(0, 5, 0);
+
+	void Update() {
+		if (Input.GetButton("Left")) {
+			direction = rotationLeft * direction;
+		}
+		if (Input.GetButton("Right")) {
+			direction = rotationRight * direction;
+		}
+        transform.position += velocity * direction;
+	}
+}
---
+++ Assets/Follow.cs
@@ -0,0 +1,17 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Follow : MonoBehaviour {
+
+    public Transform target;
+    public float maxDistance = 1.5f;
+
+    private float distance;
+
+    void Update() {
+        distance = Vector3.Distance(transform.position, target.position);
+        if (distance > maxDistance) {
+            transform.position = Vector3.MoveTowards(transform.position, target.position, distance - maxDistance);
+        }
+    }
+}
