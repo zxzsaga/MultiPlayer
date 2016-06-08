2aa021056a79f0746591102a5e293b1b8acc7a66
Xuanzhi Zhang <xuanzhi.zhang@dena.com>
2016-06-08 18:17:19 +0800

    添加 GameState

---
+++ Assets/Scripts/GameState.cs
@@ -0,0 +1,51 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class GameState : MonoBehaviour {
+
+    public int playerCount = 0;
+    public int resistanceCount = 0;
+    public int spyCount = 0;
+    public int roundMax = 0;
+    public int round = 0;
+    public int missionLeader = 0;
+    public string playerName = "";
+
+    private PlayMakerFSM playMakerFSM;
+
+    void Start() {
+        playMakerFSM = GetComponent<PlayMakerFSM>();
+    }
+
+    public void SetPlayerCount(int count) {
+        playerCount = count;
+    }
+    public void SetResistanceCount(int count) {
+        resistanceCount = count;
+    }
+    public void SetSpyCount(int count) {
+        spyCount = count;
+    }
+    public void SetRoundMax(int round) {
+        roundMax = round;
+    }
+    public void SetRound(int round) {
+        this.round = round;
+    }
+    public void SetMissionLeader(int leader) {
+        missionLeader = leader;
+    }
+
+    public void ToNextRound() {
+        round += 1;
+    }
+    public void PassLeaderShipToNextOne() {
+        missionLeader += 1;
+        missionLeader %= playerCount;
+    }
+
+    public void SetPlayerName(string name) {
+        playerName = name;
+        playMakerFSM.SendEvent("InputNameOver");
+    }
+}
